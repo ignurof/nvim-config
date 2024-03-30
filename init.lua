a8f4f103e75141eb7f4d4c26a6a4f9b591671406
ignurof <ignurof@ignurof.com>
2024-03-30 23:04:09 +0100

    Add telescope to init.lua

--- init.lua
+++ init.lua
@@ -111,4 +111,13 @@ require("lazy").setup({
 			})
 		end,
 	},
+
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', "<leader>pv", builtin.find_files, {})
+        end,
+    },
 })
