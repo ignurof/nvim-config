800e168fe791721fc1a922d6bea39c8b22693648
ignurof <ignurof@ignurof.com>
2024-05-10 08:16:32 +0200

    Add LSP and TreeSitter

--- init.lua
+++ init.lua
@@ -72,4 +72,29 @@ require("lazy").setup({
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         end,
     },
+
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
 })
+
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        -- For example, a handler override for the `rust_analyzer`:
+        -- ["rust_analyzer"] = function ()
+        --    require("rust-tools").setup {}
+        -- end
+}
