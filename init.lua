0c76784cc9652021650be804460c566a782fcebb
ignurof <ignurof@ignurof.com>
2024-05-10 19:18:31 +0200

    Fix treesitter

--- init.lua
+++ init.lua
@@ -83,16 +83,7 @@ require("lazy").setup({
 })
 
 require('nvim-treesitter.configs').setup {
-    highlight = { enable = true, additional_vim_regex_highlighting = false },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gnn", -- set to `false` to disable one of the mappings
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-        }
-    }
+    highlight = { enable = true, additional_vim_regex_highlighting = false }
 }
 
 require('mason').setup()
